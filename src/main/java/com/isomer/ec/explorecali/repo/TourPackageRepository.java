90e2a4718c0c4c6b5bffb669482ad4176fac0d9d
isomer04 <43922158+isomer04@users.noreply.github.com>
2023-10-25 18:42:52 -0400

    Worked on exposing RestFul APIs with Spring Data REST

--- src/main/java/com/isomer/ec/explorecali/repo/TourPackageRepository.java
+++ src/main/java/com/isomer/ec/explorecali/repo/TourPackageRepository.java
@@ -2,9 +2,40 @@ package com.isomer.ec.explorecali.repo;
 
 import com.isomer.ec.explorecali.domain.TourPackage;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.Optional;
 
+@RepositoryRestResource(collectionResourceRel = "packages", path="packages")
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
     Optional<TourPackage> findByName(String name);
+
+    @Override
+    @RestResource
+    <S extends TourPackage> S save(S entity);
+
+    @Override
+    @RestResource
+    <S extends TourPackage> Iterable<S> saveAll(Iterable<S> entities);
+
+    @Override
+    @RestResource
+    void deleteById(String s);
+
+    @Override
+    @RestResource
+    void delete(TourPackage entity);
+
+    @Override
+    @RestResource
+    void deleteAllById(Iterable<? extends String> strings);
+
+    @Override
+    @RestResource
+    void deleteAll(Iterable<? extends TourPackage> entities);
+
+    @Override
+    @RestResource
+    void deleteAll();
 }
